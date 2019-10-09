a0cdf90eaa5d25cb8053e6d7e0e6454e52993388
trumpe <48030821+trumpe@users.noreply.github.com>
2019-10-09 17:10:02 +0200

    Notes

---
+++ Notes.Api/SEDC.Notes/Services/Helpers/AppSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services.Helpers
+{
+    public class AppSettings
+    {
+        public string NoteAppConnectionString { get; set; }
+        public string Secret { get; set; }
+        public string TestDataApi { get; set; }
+    }
+}
